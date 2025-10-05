d6f1d56c369db6df8ca27591ce3787fe4b9d1e44
sirisha14 <sirishaaddurisiri@gmai.com>
2025-10-05 23:09:27 +0530

    asdfghjk

--- EmployeeManagment/src/main/java/com/example/EmployeeManagment/EmployeeManagmentApplication.java
+++ EmployeeManagment/src/main/java/com/example/EmployeeManagment/EmployeeManagmentApplication.java
@@ -9,5 +9,6 @@ public class EmployeeManagmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagmentApplication.class, args);
 	}
+    System.out.println("hello siri");
 
 }
